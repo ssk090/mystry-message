def9a3be18da1934082e567f4042985e1ee58bea
ssk090 <shivanandasai.38@gmail.com>
2024-04-11 15:50:29 +0530

    feat: DB Connection

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already Connected to DB");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI || "", {});
+
+    connection.isConnected = db.connections[0].readyState;
+    console.log("DB Connected Successfully!");
+  } catch (error) {
+    console.error("DB Connection Failed! ::", error);
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
